208de6dc0d9bb1d0a6b2c4385ebe114f9a5bf39c
Senua <chc0089@gmail.com>
2023-12-08 22:02:58 +0100

    Initial commit : Added auth

---
+++ src/java/Servlets/ValidarUsuario.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import Conexion.Conexion;
+import Entity.Usuario;
+import Handlers.UsuarioDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author SzBel
+ */
+@WebServlet(name = "ValidarUsuario", urlPatterns = {"/ValidarUsuario"})
+public class ValidarUsuario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        //Obtengo los parámetros del login
+        String nombre = request.getParameter("usuario");
+        String contrasenia = request.getParameter("password");
+        boolean encontrado;
+
+        //Llamo al método de obtener usuarios
+        Conexion miConexion = new Conexion();
+        miConexion.conectar();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        ArrayList<Usuario> misUsuarios = usuarioDAO.obtenerUsuarios();
+
+        encontrado = usuarioDAO.validarCredenciales(nombre, contrasenia);
+
+        if (encontrado) {
+            //Establezco los atributos de sesión
+            HttpSession session = request.getSession();
+            session.setAttribute("usuarioLogueado", true);
+            session.setAttribute("username", nombre);
+
+            Usuario usuario = usuarioDAO.obtenerUsuarioPorNombre(nombre);
+
+            //Consulto los demás datos del usuario y los añado a la sesión
+            if (usuario != null) {
+                session.setAttribute("id_usuario", usuario.getId_usuario());
+                session.setAttribute("esAdministrador", usuario.isEs_administrador());
+                session.setAttribute("saldo", usuario.getSaldo());
+
+                //Compruebo si es administrador
+                if (usuario.isEs_administrador()) {
+                    response.sendRedirect("menuAdmin.jsp");
+                } else {
+                    response.sendRedirect("menuUsuario.jsp");
+                }
+            }
+        } else {
+            out.println("El usuario no existe");
+        }
+
+        miConexion.desconectar();
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        //out.println("Método no permitido");
+        response.sendRedirect("login.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
